d98fc78ba4f25402b41c9de6835adb65383c6794
Iris Li Jingyu <iris.lijingyu@gmail.com>
2018-06-04 16:13:38 +0800

    dialog - sign-off/reject

--- ui/src/components/SidePanel.js
+++ ui/src/components/SidePanel.js
@@ -1,7 +1,18 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import {Button, ButtonGroup, DropdownButton, MenuItem, Panel} from "react-bootstrap";
+import {
+    Button,
+    ButtonGroup,
+    DropdownButton,
+    Form,
+    FormControl,
+    FormGroup,
+    MenuItem,
+    Modal,
+    Panel
+} from "react-bootstrap";
 import * as html2canvas from "html2canvas";
+import "../side-panel.css";
 
 
 class SidePanel extends Component {
@@ -13,51 +24,97 @@ class SidePanel extends Component {
         this.monitorEvent = this.monitorEvent.bind(this);
         this.stopEventMonitor = this.stopEventMonitor.bind(this);
         this.capture = this.capture.bind(this);
+        this.complete = this.complete.bind(this);
+        this.sendEvidence = this.sendEvidence.bind(this);
+        this.handleShowDialog = this.handleShowDialog.bind(this);
+        this.handleCloseDialog = this.handleCloseDialog.bind(this);
 
         this.state = {
-            started: false
+            started: false,
+            showDialog: false,
+            testPassed: undefined
         };
     }
 
     render() {
         return (
-            <Panel defaultExpanded style={{
-                "width": "100%"
-            }}>
-                {this.state.started ?
-                    <Panel.Heading>
-                        <Panel.Title toggle>
-                            <div>
-                                <p>Please note that all your actions will be logged from now</p>
-                            </div>
-                        </Panel.Title>
-                    </Panel.Heading>
-                    : null}
-                {this.state.started ?
-                    <Panel.Collapse>
-                        <Panel.Body id="logs">
-                            <div id="date">{new Date().toLocaleDateString()}</div>
-                        </Panel.Body>
-                    </Panel.Collapse>
-                    : null}
-                <Panel.Footer>
-                    <ButtonGroup vertical style={{"width": "100%"}}>
-                        {this.state.started ?
-                            <Button bsStyle="default" id="capture" onClick={this.capture}>Capture Screen</Button>
-                            : null}
-                        {this.state.started ?
-                            <DropdownButton bsStyle="success" title="Complete Monitoring" id="complete-monitoring">
-                                <MenuItem eventKey="signOff">Sign Off</MenuItem>
-                                <MenuItem eventKey="reject">Reject</MenuItem>
-                            </DropdownButton>
-                            : null}
-                        <Button
-                            onClick={this.start}
-                            bsStyle={this.state.started ? "danger" : "success"}>
-                            {(!this.state.started ? "Start" : "Stop") + " Monitoring"}</Button>
-                    </ButtonGroup>
-                </Panel.Footer>
-            </Panel>);
+            <div>
+                <Panel defaultExpanded style={{
+                    "width": "100%"
+                }}>
+                    {this.state.started ?
+                        <Panel.Heading>
+                            <Panel.Title toggle>
+                                <div>
+                                    <p>Please note that all your actions will be logged from now</p>
+                                </div>
+                            </Panel.Title>
+                        </Panel.Heading>
+                        : null}
+                    {this.state.started ?
+                        <Panel.Collapse>
+                            <Panel.Body id="logs">
+                                <div id="date">{new Date().toLocaleDateString()}</div>
+                            </Panel.Body>
+                        </Panel.Collapse>
+                        : null}
+                    <Panel.Footer>
+                        <ButtonGroup vertical style={{"width": "100%"}}>
+                            {this.state.started ?
+                                <Button bsStyle="default" id="capture" onClick={this.capture}>Capture Screen</Button>
+                                : null}
+                            {this.state.started ?
+                                <DropdownButton bsStyle="success" title="Complete Monitoring" id="complete-monitoring">
+                                    <MenuItem eventKey="signOff" onClick={() => this.complete(true)}>Sign Off</MenuItem>
+                                    <MenuItem eventKey="reject" onClick={() => this.complete(false)}>Reject</MenuItem>
+                                </DropdownButton>
+                                : null}
+                            <Button
+                                onClick={this.start}
+                                bsStyle={this.state.started ? "danger" : "success"}>
+                                {(!this.state.started ? "Start" : "Stop") + " Monitoring"}</Button>
+                        </ButtonGroup>
+                    </Panel.Footer>
+                </Panel>
+                <Modal show={this.state.showDialog} onHide={this.handleCloseDialog}
+                       dialogClassName="show-dialog-full-height">
+                    <Modal.Dialog style={{"height": "100%"}}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>
+                                <p>Confirm {this.state.testPassed ?
+                                    <span style={{"color": "green"}}>sign-off</span> :
+                                    <span style={{"color": "red"}}>rejection</span>} for JIRA:</p>
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <FormGroup
+                                    controlId="jiraKey"
+                                >
+                                    <FormControl
+                                        inputRef={node => this.jiraKey = node}
+                                        type="text"
+                                        placeholder="Enter JIRA-KEY"
+                                        required
+                                    />
+                                </FormGroup>
+                                <FormGroup
+                                    controlId="comment"
+                                >
+                                    <FormControl
+                                        inputRef={node => this.comment = node}
+                                        componentClass="textarea"
+                                        placeholder="If you have any comments..."
+                                    />
+                                </FormGroup>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button onClick={this.sendEvidence}>Send Testing Evidence</Button>
+                        </Modal.Footer>
+                    </Modal.Dialog>
+                </Modal>
+            </div>);
     }
 
     start() {
@@ -74,6 +131,7 @@ class SidePanel extends Component {
     eventHandler(e) {
         let newDescriptionCell = document.createElement("td");
         newDescriptionCell.style.width = "90%";
+        newDescriptionCell.title = e.type + " " + e.target.nodeName + ": " + e.target.outerHTML;
         newDescriptionCell.innerHTML = "<span>" + e.type + " " + e.target.nodeName + ": " + e.target.outerHTML + "</span>";
 
         let newIcon = document.createElement("span");
@@ -140,6 +198,59 @@ class SidePanel extends Component {
                 screenCaptureDiv.appendChild(screenCapture);
                 document.getElementById("logs").appendChild(screenCaptureDiv);
             });
+    }
+
+    complete(testPassed) {
+        this.setState({testPassed: testPassed});
+        this.handleShowDialog();
+    }
+
+    sendEvidence() {
+        this.jiraKey.value;
+        this.comment.value;
+
+        let logs = this.prepareLogs();
+        let statusName = "Done";
+
+        // PUT /rest/api/2/issue/{issueIdOrKey}
+        // edit status
+        // edit description as logs
+        // edit title to "Signed-Off" or "Tested and Rejected"
+
+        // POST /rest/api/2/issue/{issueIdOrKey}/comment
+        // add comment
+
+        if (this.state.testPassed) {
+        } else {
+            // PUT /rest/api/2/issue/{issueIdOrKey}/assignee
+            // assign to dev
+        }
+
+        this.handleCloseDialog();
+    }
+
+    handleShowDialog() {
+        this.setState({showDialog: true});
+    }
+
+    handleCloseDialog() {
+        this.setState({showDialog: false});
+    }
+
+    prepareLogs() {
+        let logItems = document.getElementById("logs").children;
+        let logItemsToSend = [];
+        for (let i = 0; i < logItems.length; i++) {
+            let logItem = logItems[i];
+            let logItemToSend;
+            if (logItem.classList.contains("panel")) {
+                logItemToSend = logItem.children[0].children[0].children[0].children[0].children[1].title;
+            } else {
+                logItemToSend = logItem.innerHTML;
+            }
+            logItemsToSend.push(logItemToSend);
+        }
+        return logItemsToSend;
     }
 }
 
